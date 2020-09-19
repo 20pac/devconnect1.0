898d9172080db6da788d83124fd7e6f607332993
gr8fuldead <ananyadoesit@gmail.com>
2020-09-19 20:13:10 +0530

    Setup Redux

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { Login } from "./components/layout/auth/Login";
+import { Register } from "./components/layout/auth/Register";
+import { Landing } from "./components/layout/Landing";
+import { Navbar } from "./components/layout/Navbar";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
